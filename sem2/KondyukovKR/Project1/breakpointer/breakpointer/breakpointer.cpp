9442225669d883a1c9b96ab0e3cf4262b99217a7
Konstantin Kondyukov <k.r.kondiukov@foxford.ru>
2024-04-24 12:52:39 +0300

    Kostya branch

---
+++ sem2/KondyukovKR/Project1/breakpointer/breakpointer/breakpointer.cpp
@@ -0,0 +1,11 @@
+﻿// breakpointer.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	__asm int 0x0E;
+	printf("Hello World\n");
+	return 0;
+}
