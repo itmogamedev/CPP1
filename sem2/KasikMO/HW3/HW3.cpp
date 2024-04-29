cdfa9640bd764ec12b521fb43c6cdfef6038b4ef
Касик Максим Олегович <58136887+Irbis888@users.noreply.github.com>
2024-04-29 14:17:37 +0300

    Kasik Maxim HW3 ver. 2

--- sem2/KasikMO/HW3/HW3.cpp
+++ sem2/KasikMO/HW3/HW3.cpp
@@ -2,11 +2,10 @@
 #include <iostream>
 #include "windows.h"
 
-using namespace sf;
 
 int clickAnalyze(int x, int y) { // 112, 286; y = 120, 280
     // I would make a decent click tracking but it's too minor app to bother
-    if (x > 112 && x < 286) { 
+    if (x > 112 && x < 286) {
         if (y > 120 && y < 280) {
             return 1;
         }
@@ -30,29 +29,29 @@ int clickAnalyze(int x, int y) { // 112, 286; y = 120, 280
 int main()
 {
     std::srand(std::time(nullptr));
-    int badButton = (std::rand()%4)+1;
+    int badButton = (std::rand() % 4) + 1;
     int kill = false;
     //std::cout << badButton;
-    RenderWindow window(VideoMode(800, 800), L"Испытай удачу!", Style::Default);
-    CircleShape shape(100.f, 6);
+    sf::RenderWindow window(sf::VideoMode(800, 800), L"Испытай удачу!", sf::Style::Default);
+    sf::CircleShape shape(100.f, 6);
     sf::Font font;
     font.loadFromFile("tuffy.ttf");
-    Text t;
-    String s = "Pick a button, challenge your luck!";
+    sf::Text t;
+    sf::String s = "Pick a button, challenge your luck!";
     t.setFont(font);
     t.setString(s);
-    t.setFillColor(Color::White);
+    t.setFillColor(sf::Color::White);
     t.setCharacterSize(24);
-    
+
 
     while (window.isOpen())
     {
-        Event event;
+        sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == Event::Closed)
+            if (event.type == sf::Event::Closed)
                 window.close();
-            if (event.type == Event::MouseButtonReleased) {
+            if (event.type == sf::Event::MouseButtonReleased) {
                 //std::cout << clickAnalyze(event.mouseButton.x, event.mouseButton.y) << "\n";
                 if (clickAnalyze(event.mouseButton.x, event.mouseButton.y) == badButton) {
                     s = "Goodbye))";
@@ -62,9 +61,9 @@ int main()
             }
 
         }
-        window.clear(Color::Black);
+        window.clear(sf::Color::Black);
         shape.setPosition(100, 100);
-        shape.setFillColor(Color(0xFF, 0xBF, 0x40, 255));
+        shape.setFillColor(sf::Color(0xFF, 0xBF, 0x40, 255));
         window.draw(shape);
 
         shape.setPosition(100, 400);
@@ -79,7 +78,7 @@ int main()
         t.setString(s);
         window.draw(t);
         window.display();
-        
+
         if (kill) {
             int delay = (rand() % 50000) + 10000; // number of milliseconds between 10000 and 60000
             //std::cout << delay;
