607c9645dd78880ac5b261591104c1f1e620df31
Firmus Fire <62419071+Strizhov-Dmitry-Andreevich@users.noreply.github.com>
2024-06-18 06:01:57 +0300

    123

---
+++ sem2/StrizhovDA/Algos/Algos/Algos.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <map>
+
+int main() {
+    int n, p;
+    std::map<int, int> m_IntToInt;
+    std::cin >> n;
+    std::cin >> p;
+    m_IntToInt[p] = p;
+    m_IntToInt[1e18] = p;
+    for (int i = 2; i <= n; i++) {
+        int q;
+        std::cin >> q;
+        auto w = m_IntToInt.lower_bound(q);
+        std::cout << (*w).second << " ";
+        m_IntToInt[q] = (*w).second = q;
+    }
+}
